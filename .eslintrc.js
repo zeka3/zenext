6ea4c0e4213ad631b943069d18e932eb620b7b32
R. Taha BAŞ <rtahabas@gmail.com>
2023-08-13 18:23:54 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,13 +70,7 @@ module.exports = {
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
       },
     ],
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
+    'react/function-component-definition': 'off',
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
     'jsx-a11y/click-events-have-key-events': 'off',
